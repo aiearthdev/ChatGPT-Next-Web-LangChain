7f6755ad19ae1dd98e5b24f4f0906bd1d9f71ddf
aiearthdev <aiearthhub@proton.me>
2024-08-03 15:56:06 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,6 +1,6 @@
 export const OWNER = "Hk-Gosuto";
 export const REPO = "ChatGPT-Next-Web-LangChain";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
+export const REPO_URL = `https://aio.aiearth.dev`;
 export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
 export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const RELEASE_URL = `${REPO_URL}/releases`;
@@ -8,10 +8,10 @@ export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/c
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
 
-export const DEFAULT_API_HOST = "https://api.nextchat.dev";
-export const OPENAI_BASE_URL = "https://api.openai.com";
-export const GEMINI_BASE_URL = "https://generativelanguage.googleapis.com/";
-export const ANTHROPIC_BASE_URL = "https://api.anthropic.com";
+export const DEFAULT_API_HOST = "https://maxapi.aiearth.dev";
+export const OPENAI_BASE_URL = "https://maxapi.aiearth.dev";
+export const GEMINI_BASE_URL = "https://maxapi.aiearth.dev";
+export const ANTHROPIC_BASE_URL = "https://maxapi.aiearth.dev";
 
 export const BAIDU_BASE_URL = "https://aip.baidubce.com";
 export const BAIDU_OATUH_URL = `${BAIDU_BASE_URL}/oauth/2.0/token`;
